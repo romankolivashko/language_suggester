13d1d25fd16b68ae6ab0465d6a7eb2341a09b0aa
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-13 12:21:17 -0700

    Redone business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,15 @@
 
-// Engineering: Objective-C, Swift
-// Banking and Finance: Python
-// Marketing: SQL
-// Manufacturing: C#
-// Retail: JavaScript
-// Security: C
-// Media: C++, JS
-// Healthcare: Java
-// Science: MATLAB
+// python -person
+// c#     -person
+// ruby   -person
 
-//let lang;
-// let genStatement = "JavaScript, Python, Swift"; 
-// let mobileNot = "C++";
-// // if (industry && person && mobile = "yes") {
-// //      then 
-// //      return industry + person + mobile lang;
-//         $(".firstLastName").text(name);
-//         $(".industry").text(industry);
-//         $(".person").text(person);
-//         $(".mobile").text(mobile);
-// //    }
-// // else if (industry && person  && mobile = "no")
-// //    {
-// //      return industry + person + "C++"; //great for beginners
-//         $(".firstLastName").text(name);
-//         $(".industry").text(industry);
-//         $(".person").text(person);
-//         $(".mobile").text(mobileNot);
-// //    }
-// // else {
-// //      return genStatement;
-// // }
-   
-         
+// javascript -industry
+// C          -industry
+// java       -industry 
 
-
-
-
-
+// Obj-C      -mobile
+// Swift      -mobileMaybe
+// C++        -general good to know
 
 
 
@@ -47,15 +19,39 @@ $(document).ready(function() {
     $(".card-hidden").show();
 
     const name = $("input#firstLastName").val();
-    const industry = $("select#industry").val();
-    const person = $("select#person").val();
-    const mobile = $("input:radio[name=mobile]:checked").val();
+    let industry = $("select#industry").val();
+    let person = $("select#person").val();
+    let mobile = $("input:radio[name=mobile]:checked").val();
+    let industryLang;
+
+    if (industry === "Banking and Finance") {
+      industryLang = "Python";
+    } else if (industry === "Manufacturing"){
+       industryLang = "C#"; 
+    } else if (industry === "Retail"){
+       industryLang = "JavaScript"; 
+    }  else if (industry === "Media"){
+       industryLang = "JavaScript"; 
+    }  else if (industry === "Healthcare"){
+       industryLang = "Java"; 
+    } 
+
+    if (person === "Jack Dorsey") {
+      personLang = "Ruby";
+    } else if (person === "Sergey Brin"){
+      personLang = "Python"; 
+    } else if (person === "Bill Gates"){
+      pesrsonLang = "C#"; 
+    } 
+
 
     $(".firstLastName").text(name);
     $(".industry").text(industry);
     $(".person").text(person);
     $(".mobile").text(mobile);
     $(".genStatement").hide();
+    $(".personLang").text(personLang);
+    $(".industryLang").text(industryLang);
 
   });
 });
@@ -63,55 +59,41 @@ $(document).ready(function() {
 
 
 
+// $(document).ready(function() {
+//   $(".btn").click(function() {
+//     $(".card-showing").hide();
+//     $(".card-hidden").show();
+
+//     const name = $("input#firstLastName").val();
+//     const industry = $("select#industry").val();
+//     const person = $("select#person").val();
+//     const mobile = $("input:radio[name=mobile]:checked").val();
+//     const genStatement = "JavaScript, Python, Swift"; 
+//     const mobileNot = "C++";
+//     console.log(industry);
+
+//    if (mobile === "yes") {
+//         $(".firstLastName").text(name);
+//         $(".industry").text(industry);
+//         console.log(industry);
+//         $(".person").text(person);
+//         $(".mobile").text(mobile);
+//         $(".genStatement").hide();
+//       }
+//    else if (mobile === "no")
+//       {
+//         $(".firstLastName").text(name);
+//         $(".industry").text(industry);
+//         $(".person").text(person);
+//         $(".mobile").text(mobileNot);
+//         console.log("testing mobileNot");
+//         $(".genStatement").hide(); 
+//      }
+//    else {
+//         console.log("Testing else");
+//         $(".genStatement").show();
+//    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function() {
-  $(".btn").click(function() {
-    $(".card-showing").hide();
-    $(".card-hidden").show();
-
-    const name = $("input#firstLastName").val();
-    const industry = $("select#industry").val();
-    const person = $("select#person").val();
-    const mobile = $("input:radio[name=mobile]:checked").val();
-    const genStatement = "JavaScript, Python, Swift"; 
-    const mobileNot = "C++";
-    console.log(industry);
-
-   if (mobile === "yes") {
-        $(".firstLastName").text(name);
-        $(".industry").text(industry);
-        console.log(industry);
-        $(".person").text(person);
-        $(".mobile").text(mobile);
-        $(".genStatement").hide();
-      }
-   else if (mobile === "no")
-      {
-        $(".firstLastName").text(name);
-        $(".industry").text(industry);
-        $(".person").text(person);
-        $(".mobile").text(mobileNot);
-        console.log("testing mobileNot");
-        $(".genStatement").hide(); 
-     }
-   else {
-        console.log("Testing else");
-        $(".genStatement").show();
-   }
-
-
-  });
-});
+//   });
+// });
