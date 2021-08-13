b5c478d7345a7f0c5543f18615c8e7ed94417901
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-13 11:19:49 -0700

    Business logic needs work and separation from UI

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,35 @@
 // Science: MATLAB
 
 //let lang;
-
-// if (industry && islandPerson && mobile = "yes") {
-//      then 
-//      return industry + person + mobile lang;
-//    }
-
-// else if (industry && person  && mobile = "no")
-//    {
-//      then 
-//      return industry + person + "C++"; //great for beginners
-//    }
-   
-// else {
-//      
-//      return Javascript, Python, Swift;
-// }
+// let genStatement = "JavaScript, Python, Swift"; 
+// let mobileNot = "C++";
+// // if (industry && person && mobile = "yes") {
+// //      then 
+// //      return industry + person + mobile lang;
+//         $(".firstLastName").text(name);
+//         $(".industry").text(industry);
+//         $(".person").text(person);
+//         $(".mobile").text(mobile);
+// //    }
+// // else if (industry && person  && mobile = "no")
+// //    {
+// //      return industry + person + "C++"; //great for beginners
+//         $(".firstLastName").text(name);
+//         $(".industry").text(industry);
+//         $(".person").text(person);
+//         $(".mobile").text(mobileNot);
+// //    }
+// // else {
+// //      return genStatement;
+// // }
    
          
+
+
+
+
+
+
 
 
 $(document).ready(function() {
@@ -44,8 +55,63 @@ $(document).ready(function() {
     $(".industry").text(industry);
     $(".person").text(person);
     $(".mobile").text(mobile);
-
+    $(".genStatement").hide();
 
   });
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $(".btn").click(function() {
+    $(".card-showing").hide();
+    $(".card-hidden").show();
+
+    const name = $("input#firstLastName").val();
+    const industry = $("select#industry").val();
+    const person = $("select#person").val();
+    const mobile = $("input:radio[name=mobile]:checked").val();
+    const genStatement = "JavaScript, Python, Swift"; 
+    const mobileNot = "C++";
+    console.log(industry);
+
+   if (mobile === "yes") {
+        $(".firstLastName").text(name);
+        $(".industry").text(industry);
+        console.log(industry);
+        $(".person").text(person);
+        $(".mobile").text(mobile);
+        $(".genStatement").hide();
+      }
+   else if (mobile === "no")
+      {
+        $(".firstLastName").text(name);
+        $(".industry").text(industry);
+        $(".person").text(person);
+        $(".mobile").text(mobileNot);
+        console.log("testing mobileNot");
+        $(".genStatement").hide(); 
+     }
+   else {
+        console.log("Testing else");
+        $(".genStatement").show();
+   }
+
+
+  });
+});
