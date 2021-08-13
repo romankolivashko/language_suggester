2531b3db38625bb4ba075d444838f4594162d286
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-13 13:14:51 -0700

    Testing returns positive results, there is a need for Learn More links

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-
-// python -person
-// c#     -person
-// ruby   -person
-
+//Language use legend
+//---------------------------------
+// python     -person
+// c#         -person
+// ruby       -person
 // javascript -industry
 // C          -industry
 // java       -industry 
-
 // Obj-C      -mobile
 // Swift      -mobileMaybe
 // C++        -general good to know
@@ -18,14 +17,20 @@ $(document).ready(function() {
     $(".card-showing").hide();
     $(".card-hidden").show();
 
-    const name = $("input#firstLastName").val();
+    let name = $("input#firstLastName").val();
     let industry = $("select#industry").val();
     let person = $("select#person").val();
     let mobile = $("input:radio[name=mobile]:checked").val();
     let industryLang;
+    let url;
+
+    if (name === "") {
+      name = "Beautiful Stranger";
+    }
 
     if (industry === "Banking and Finance" || industry === "Engineering") {
       industryLang = "C";
+      // url  = "https://www.w3schools.com";
     } else if (industry === "Manufacturing" || industry === "Healthcare"){
        industryLang = "Java"; 
     } else if (industry === "Retail" || industry === "Media"){
@@ -50,6 +55,7 @@ $(document).ready(function() {
       mobileLang = "C++" //another great general purpose language
     }
     else {
+      mobile = "somewhat interested";
       mobileLang = "Swift";
     }
 
@@ -60,46 +66,8 @@ $(document).ready(function() {
     $(".mobile").text(mobile);
     $(".personLang").text(personLang);
     $(".industryLang").text(industryLang);
+    // $(".industryLangLearnMore").text(url);
     $(".mobileLang").text(mobileLang);
   });
 });
 
-
-// $(document).ready(function() {
-//   $(".btn").click(function() {
-//     $(".card-showing").hide();
-//     $(".card-hidden").show();
-
-//     const name = $("input#firstLastName").val();
-//     const industry = $("select#industry").val();
-//     const person = $("select#person").val();
-//     const mobile = $("input:radio[name=mobile]:checked").val();
-//     const genStatement = "JavaScript, Python, Swift"; 
-//     const mobileNot = "C++";
-//     console.log(industry);
-
-//    if (mobile === "yes") {
-//         $(".firstLastName").text(name);
-//         $(".industry").text(industry);
-//         console.log(industry);
-//         $(".person").text(person);
-//         $(".mobile").text(mobile);
-//         $(".genStatement").hide();
-//       }
-//    else if (mobile === "no")
-//       {
-//         $(".firstLastName").text(name);
-//         $(".industry").text(industry);
-//         $(".person").text(person);
-//         $(".mobile").text(mobileNot);
-//         console.log("testing mobileNot");
-//         $(".genStatement").hide(); 
-//      }
-//    else {
-//         console.log("Testing else");
-//         $(".genStatement").show();
-//    }
-
-
-//   });
-// });
